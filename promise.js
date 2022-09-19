8995e54177f195763df6f78c201cef11068d74be
delfia <delfiadoank@gmail.com>
2022-09-19 22:15:21 +0700

    Tugas Promises

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+
+  var ixx = await promiseTheaterIXX();
+  var vgc = await promiseTheaterVGC();
+
+  var all = ixx.concat(vgc);
+
+  let count = 0;
+
+  all.forEach(element => {
+    if(element.hasil == emosi) count++;
+  });
+
+  console.log("emosi: ", emosi, " => ", count)
+
+  return count;
+};
 
 module.exports = {
   promiseOutput,
